8723f1a78aa1cc1613f81d81ce87d831944f598c
jsilva3 <jsilva3@gmail.com>
2016-06-29 16:50:11 -0400

    verify setlist

--- app/setlist/setlist.js
+++ app/setlist/setlist.js
@@ -9,38 +9,45 @@ angular.module('myApp.setlist', ['ngRoute','firebase','ngAnimate'])
     controller: 'setlistCtrl'
   });
 }])
-.factory('setlistService', function($http) {
-    return {
-      getSetlist: function() {
-         return $http.get('https://api.phish.net/api.js?showdate=06%2F22%2F2016&api=2.0&format=json&method=pnet.shows.setlists.get&artist=1');
-      }
-    }
-  })
+
 .controller('setlistCtrl', setlistCtrl); 
 
-function setlistCtrl($timeout, $q, $log, $scope, $location, $firebaseArray,$firebaseObject, setlistService) {
+function setlistCtrl($timeout, $q, $log, $scope, $location, $firebaseArray,$firebaseObject) {
     var self = this;
 
-function myLoad(){
-       var promise = 
-          setlistService.getSetlist()
-          promise.then(
-          function(payload) { 
-            var setlistData1=[];
-              setlistData1 = payload.data[0];
-              $scope.setlist = setlistData1.map( function (song) {
-              return song;
-          });
-          },
-          function(errorPayload) {
-              $log.error('failure loading song', errorPayload);
-          });
-          console.log("here");
+    //for postgres
+
+function loadPlayed() {
+  var tempObj = {};
+  var playedRef = firebase.database().ref("played/");
+  return $firebaseArray(playedRef)
+};
+$scope.played = loadPlayed();
+
+function loadSongs(showid) {
+  var songsRef = firebase.database().ref("played/" + showid);
+  $scope.songs = $firebaseArray(songsRef);
+  console.log("here");
+  return;
 };
 
-//$timeout(function() {
-// console.log ($scope.setlist)
-//},3000);
+      $scope.selectedItem;
+      $scope.getSelectedText = function() {
+        if ($scope.selectedItem !== undefined) {
+          console.log ("You have selected: Item " + $scope.selectedItem);
+          var songsRef = firebase.database().ref("played/" + $scope.selectedItem);
+          $scope.songs = $firebaseArray(songsRef);
+          return "Show " + $scope.selectedItem;
+        } else {
+          return "Please select a show";
+        }
+      };
+
+
+
+$timeout(function() {
+ console.log ($scope.played);
+},2000);
 
 };
 })();
